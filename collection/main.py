adcf5edcfad50f3690c18945df1e64c25fb5f00d
Anis <anis2k20@gmail.com>
2023-10-04 01:02:33 +0600

    Problem Solved.

--- collection/main.py
+++ collection/main.py
@@ -1,5 +1,7 @@
 def purchase(customer,size):
     customer_choice = {}
+    # customer_choice = {'6': [55, 45, 55], '4': [40], '18': [60]}
+    output = 0
     for i in range(1,customer+1):
         s = int(input())
         p = int(input())
@@ -8,7 +10,23 @@ def purchase(customer,size):
                 customer_choice.setdefault(f"{s}",[]).append(p)
             else:
                 customer_choice.setdefault(f"{s}",[]).extend(p)
-    print(customer_choice)
+
+    cus_selection = customer_choice.keys()
+    cus_selection_dict = {}
+    for i in cus_selection:
+        cus_selection_dict[i]=size.count(int(i))
+
+    l = []
+    for i in cus_selection_dict.values():
+        l.append(i)
+
+    x=0
+    for i in customer_choice.values():
+        for j in range(l[x]):
+            output+=i[j]
+        x+=1
+
+    print(output)
 
 
 num_of_shoes = 10
