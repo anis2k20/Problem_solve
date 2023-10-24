e9eaba012d7352d99a0fdad56fc189609db0b18d
Anis <anis2k20@gmail.com>
2023-10-24 13:58:38 +0600

    prime number series print

--- prime_number/main.py
+++ prime_number/main.py
@@ -1,15 +1,15 @@
 # Python program to print all Prime numbers in an Interval
 
-num = 12
+num = 100
 prime_nums=[]
 
-for i in range(1,num+1):
-    if i>=3:
+for i in range(2,num+1):
+    if i>=2:
+        flag=0
         for j in range(2,i):
-            if i%j==0:
-                print("not prime")
-                continue
-            else:
-                prime_nums.append(i)
+           if i%j==0:
+               flag=1
+        if flag==0:
+            prime_nums.append(i)
 
 print(prime_nums)
